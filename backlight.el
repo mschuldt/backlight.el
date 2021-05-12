2675cd65d86755ef375037f70cf599c7f4f1e78a
mschuldt <mbschuldt@gmail.com>
2021-05-11 18:24:11 -0700

    add udev note to commentary section

--- backlight.el
+++ backlight.el
@@ -30,6 +30,11 @@
 ;; controls but allows for increased resolution when the
 ;; brightness percentage nears zero.
 ;;
+;; On some systems a udev rule must be added, in /etc/udev/rules.d/backlight.rules add:
+;;      ACTION=="add", SUBSYSTEM=="backlight", KERNEL=="acpi_video0", GROUP="video", MODE="then"
+;; then reload with:
+;;     sudo udevadm control --reload-rules && udevadm trigger
+;;
 ;; USAGE
 ;;
 ;;  M-x backlight
